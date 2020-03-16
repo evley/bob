2ee68a4f3295f2ac27f27b3a013c7516ba4aea78
tinybigideas <craig@tinybigideas.com>
2020-03-16 08:16:12 +0000

    Add virtual scrolling

--- src/app/view/+data-list/data-list.component.ts
+++ src/app/view/+data-list/data-list.component.ts
@@ -10,4 +10,48 @@ import { DataListItem } from './data-list-item.interface';
 export class DataListComponent {
   @Input() public name: string;
   @Input() public items: DataListItem[] = [];
+
+  public get addedCount(): string {
+    return `${this.items.filter((i) => this.hasAddedDate(i)).length}/${this.items.length}`;
+  }
+
+  public hasAddedDate(item: DataListItem): boolean {
+    return item.added && item.added instanceof Date;
+  }
+
+  public isExpired(item: DataListItem): boolean {
+    return this._hasExpiryDate(item) && item.expiry <= new Date();
+  }
+
+  public isUnchecked(item: DataListItem): boolean {
+    return this._hasCheckedDate(item) && this._todaySubtractDays(-90) < item.checked;
+  }
+
+  public isExpiring(item: DataListItem): boolean {
+    return this._hasExpiryDate(item) && this._todaySubtractDays(-90) > item.expiry;
+  }
+
+  public hasCalories(item: DataListItem): boolean {
+    return item.calories && item.calories > 0;
+  }
+
+  public hasWater(item: DataListItem): boolean {
+    return item.water && item.water > 0;
+  }
+
+  public getIcon(item: DataListItem): string {
+    return item.water > item.calories ? 'mdi-cup-water' : 'mdi-food-apple';
+  }
+
+  private _hasExpiryDate(item: DataListItem): boolean {
+    return item.expiry && item.expiry instanceof Date;
+  }
+
+  private _hasCheckedDate(item: DataListItem): boolean {
+    return item.checked && item.checked instanceof Date;
+  }
+
+  private _todaySubtractDays(days: number): Date {
+    return new Date(new Date().setDate(new Date().getDate() - days));
+  }
 }
--- src/app/view/+data-list/data-list.module.ts
+++ src/app/view/+data-list/data-list.module.ts
@@ -1,12 +1,14 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 import { DataListComponent } from './data-list.component';
 
 @NgModule({
   declarations: [DataListComponent],
-  imports: [CommonModule, MatListModule],
+  imports: [CommonModule, MatListModule, MatIconModule, ScrollingModule],
   exports: [DataListComponent],
   providers: []
 })
